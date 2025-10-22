99fa504c2071447a08e6d43888130b7af49c4bfc
ch1naski <sik6oi@protonmail.com>
2025-10-22 22:56:37 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express();
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, '🎉 Бот запущен! Я работаю!');
+});
+
+bot.onText(/\/help/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Помощь: /start, /help');
+});
+
+bot.on('message', (msg) => {
+  if (!msg.text.startsWith('/')) {
+    bot.sendMessage(msg.chat.id, `Эхо: ${msg.text}`);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Бот запущен!');
+});
